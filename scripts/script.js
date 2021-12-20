5bbbc07cfce59cb0e8b1cb5e880105606e15fe76
ozprogz <68549135+ozprogz@users.noreply.github.com>
2021-12-20 10:20:32 +0100

    Finished the first 5 steps for completing the project. Little to no styling added

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,51 @@
+const grid = document.querySelector(".container");
 const makeGrid = (size)=>{
-    const grid = document.querySelector(".container");
+    
     for(let i = 0; i<size;i++){
-        let gridBlock = document.createElement("div");
-        gridBlock.classList.add("grid-element");
-        grid.appendChild(gridBlock);
+        let gridElement = document.createElement("div");
+        gridElement.classList.add("grid-element");
+        grid.appendChild(gridElement);
     }
 
 
 }
 let gridSize = 16*16;
 makeGrid(gridSize);
+
+const gridBlocks = document.querySelectorAll(".grid-element");
+gridBlocks.forEach( item =>{ item.addEventListener("mouseover", ()=>{
+        item.classList.add("change-color");
+    });
+});
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", ()=>{
+    gridBlocks.forEach(item =>{
+        item.classList.remove("change-color");
+        
+        
+    })
+    let entry;
+    while(1){
+        entry = prompt("What size should the grid have?");
+        if(parseInt(entry)<=100) break;
+        
+    }
+    
+    grid.innerHTML="";
+    makeGrid(entry*entry);
+    
+    grid.style.gridTemplateColumns = `repeat(${entry}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${entry}, auto)`;
+    
+    let gridBlock = document.querySelectorAll(".grid-element");
+    gridBlock.forEach( item =>{ item.addEventListener("mouseover", ()=>{
+        item.classList.add("change-color");
+    });
+});
+  
+
+
+});
+
+
