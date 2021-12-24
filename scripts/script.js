3829910b9393aaaf42e26419a4d360255cf4d9ee
ozprogz <68549135+ozprogz@users.noreply.github.com>
2021-12-24 11:33:06 +0100

    cleaned up the code by creating more functions and changed the logic a bit

--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,70 @@
-let black = true; 
+const DEFAULT_SIZE = 16;
+
+let defaultMode = true; 
+let gridSize = DEFAULT_SIZE; 
+
 const grid = document.querySelector(".container");
-const makeGrid = (size)=>{
-    
-    for(let i = 0; i<size;i++){
+const clearButton = document.querySelector(".clear");
+const rgbButton = document.querySelector(".rgb-button");
+const blackButton = document.querySelector(".black-button");
+
+makeGrid(gridSize);
+
+clearButton.addEventListener("click", ()=>{
+    clearGrid();     
+    if(defaultMode==true){
+        blackMode();
+}   
+    else rgbMode();
+});
+
+rgbButton.addEventListener("click", ()=>{
+    rgbMode();
+});
+
+
+blackButton.addEventListener("click", ()=>{
+   blackMode()
+});
+
+function addClass(){
+    const gridBlocks = document.querySelectorAll(".grid-element");
+    gridBlocks.forEach( item =>{ item.addEventListener("mouseover", ()=>{
+        item.classList.add("change-color");
+        });
+    }); 
+}
+function rgbMode(){
+    defaultMode = false;
+    const gridBlocks = document.querySelectorAll(".grid-element");
+    gridBlocks.forEach( item =>{ item.addEventListener("mouseover", ()=>{
+        setRgb(item);
+    });
+});
+}
+
+function blackMode(){
+    defaultMode = true;
+    const gridBlocks = document.querySelectorAll(".grid-element");
+    gridBlocks.forEach( item =>{ item.addEventListener("mouseover", ()=>{
+        item.style.backgroundColor = "black";
+
+})})
+
+}
+function makeGrid(size) {
+    let squared = size * size;
+    for(let i = 0; i < squared; i++){
         let gridElement = document.createElement("div");
         gridElement.classList.add("grid-element");
         grid.appendChild(gridElement);
+    
     }
-
+    addClass();
 
 }
-let gridSize = 16*16;
-makeGrid(gridSize);
 
-const gridBlocks = document.querySelectorAll(".grid-element");
-gridBlocks.forEach( item =>{ item.addEventListener("mouseover", ()=>{
-        item.classList.add("change-color");
-    });
-    });
-
-const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", ()=>{
-    gridBlocks.forEach(item =>{
-        item.classList.remove("change-color");
-        
-        
-    })
-    let entry;
-    while(1){
-        entry = parseInt(prompt("What size should the grid have? Enter a number between 1-100"));
-        if(entry!=0 &&(entry <=100)) break;
-    }
-    
-    grid.innerHTML="";
-    makeGrid(entry*entry);
-    
-    grid.style.gridTemplateColumns = `repeat(${entry}, auto)`;
-    grid.style.gridTemplateRows = `repeat(${entry}, auto)`;
-    
-    let gridBlock = document.querySelectorAll(".grid-element");
-    if(black) 
-    {   gridBlock.forEach( item =>{ item.addEventListener("mouseover", ()=>{
-        item.classList.add("change-color");
-    });
-});
-}
-    else {
-        gridBlock.forEach( item =>{ item.addEventListener("mouseover", ()=>{
-            generateRandomRgbValue(item);
-        });
-    });
-    }       
-});
-
-const rgbButton = document.querySelector(".rgb-button");
-rgbButton.addEventListener("click", ()=>{
-    black = false;
-    const test = document.querySelectorAll(".grid-element");
-    test.forEach( item =>{ item.addEventListener("mouseover", ()=>{
-        generateRandomRgbValue(item);
-
-    
-
-})})})
-
-const blackButton = document.querySelector(".black-button");
-blackButton.addEventListener("click", ()=>{
-    black = true;
-    const test = document.querySelectorAll(".grid-element");
-    test.forEach( item =>{ item.addEventListener("mouseover", ()=>{
-        item.style.backgroundColor = "black";
-    
-
-})})})
-
-
-function generateRandomRgbValue(item){
+function setRgb(item){
     let max =256;
     let r = Math.floor(Math.random() * max);
     let g = Math.floor(Math.random() * max);
@@ -83,4 +72,15 @@ function generateRandomRgbValue(item){
     item.style.backgroundColor =`rgb(${r},${g},${b})`;
 }
 
+function clearGrid(){
+    let entry;
+    while(1){
+        entry = parseInt(prompt("What size should the grid have? Enter a number between 1-100"));
+        if(entry!=0 &&(entry <=100)) break;
+    }
 
+    grid.innerHTML="";
+    makeGrid(entry);
+    grid.style.gridTemplateColumns = `repeat(${entry}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${entry}, auto)`;
+}
